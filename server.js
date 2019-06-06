3b919d69b76d15280158d7375589862784090892
Dong Beiqing <bedong@ebay.com>
2019-06-06 16:58:33 +0800

    server

---
+++ server.js
@@ -0,0 +1,52 @@
+let http = require('http');
+let fs = require('fs');
+let path = require('path');
+let mime = require('mime');
+let cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]); // read from memory
+    } else {
+        fs.exists(absPath, function (exists) {
+            if (exists) {
+                fs.readFile(absPath, function (err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        })
+    }
+}
+
+let server = http.createServer(function (request, response) {
+    let filePath = false;
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    let absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function () {
+    console.log("Server is listening on port 3000");
+});
